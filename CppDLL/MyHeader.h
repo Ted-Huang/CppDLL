ad693e36de50c5d468d614163c39c9d3c91adf7d
Ted <fw128ted@gmail.com>
2018-09-19 11:51:48 +0800

    remove project reference use pragma to link

--- CppDLL/MyHeader.h
+++ CppDLL/MyHeader.h
@@ -3,14 +3,16 @@
 #define __My_SDK_H
 
 #pragma once
+
+
 #ifdef MATHFUNCSDLL_EXPORTS
-#define MATHFUNCSDLL_API __declspec(dllexport) 
+#define MATHFUNCSDLL_API  __declspec(dllexport) 
 #else
-#define MATHFUNCSDLL_API __declspec(dllimport) 
+#define MATHFUNCSDLL_API  __declspec(dllimport) 
 #endif
 
 
- class MyClass
+class MyClass
 {
 public:
 	MATHFUNCSDLL_API static int MyPlusTen(int nInput);
--- TestDllSample/TestDllSample.cpp
+++ TestDllSample/TestDllSample.cpp
@@ -5,6 +5,14 @@
 #include "iostream"
 #include "MyHeader.h"
 
+#ifdef WIN64
+#pragma comment(lib, "..\\Debug\\CppDLL.lib")
+#else
+#pragma comment(lib, "..\\Debug\\CppDLL.lib")
+#endif
+
+
+
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
