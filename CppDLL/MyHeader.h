7feb37ef826dc60be2e1ac7956706b00db0c3bf1
Ted <fw128ted@gmail.com>
2018-09-19 11:18:12 +0800

    modify dll header modify sample use dll(use project link setting)

--- CppDLL/MyHeader.h
+++ CppDLL/MyHeader.h
@@ -1,6 +1,19 @@
 
-class MyClass
+#ifndef __My_SDK_H
+#define __My_SDK_H
+
+#pragma once
+#ifdef MATHFUNCSDLL_EXPORTS
+#define MATHFUNCSDLL_API __declspec(dllexport) 
+#else
+#define MATHFUNCSDLL_API __declspec(dllimport) 
+#endif
+
+
+ class MyClass
 {
 public:
-	static int MyPlusTen(int nInput);
+	MATHFUNCSDLL_API static int MyPlusTen(int nInput);
 };
+
+#endif
--- TestDllSample/TestDllSample.cpp
+++ TestDllSample/TestDllSample.cpp
@@ -3,12 +3,15 @@
 
 #include "stdafx.h"
 #include "iostream"
+#include "MyHeader.h"
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "start test " << endl;
+	
+	cout << MyClass::MyPlusTen(10) << endl;
 	return 0;
 }
 
