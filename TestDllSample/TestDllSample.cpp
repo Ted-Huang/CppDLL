e3354b24ae8ee4d9f7a1affb72b6a40d8767dd65
Ted <fw128ted@gmail.com>
2018-09-20 10:04:47 +0800

    test load library

--- TestDllSample/TestDllSample.cpp
+++ TestDllSample/TestDllSample.cpp
@@ -3,7 +3,8 @@
 
 #include "stdafx.h"
 #include "iostream"
-#include "MyHeader.h"
+//#include "MyHeader.h"
+#include <windows.h>
 
 #ifdef WIN64
 #pragma comment(lib, "..\\Debug\\CppDLL.lib")
@@ -14,16 +15,34 @@
 
 
 using namespace std;
+typedef int(*f_funci)(int nInput);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "start test " << endl;
 	
-	cout << MyClass::MyPlusTen(10) << endl;
-	
-	MyClassToExport* pClassExport = createMyClassToExport();
+	//cout << MyClass::MyPlusTen(10) << endl;
+	//
+	//MyClassToExport* pClassExport = createMyClassToExport();
 
-	cout << pClassExport->GetTen() << endl;
+	//cout << pClassExport->GetTen() << endl;
+
+	//test load library
+	HINSTANCE hGetProcIDDLL = LoadLibrary(_T("CppDLL.dll"));
+
+	if (!hGetProcIDDLL) {
+		std::cout << "could not load the dynamic library" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// resolve function address here
+	f_funci funci = (f_funci)GetProcAddress(hGetProcIDDLL, "Test");
+	if (!funci) {
+		std::cout << "could not locate the function" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::cout << "funci() returned " << funci(14) << std::endl;
 
 	return 0;
 }
